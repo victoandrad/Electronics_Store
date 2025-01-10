2b847d1780183cf0d5fb34e94b0650a4f5bba4c3
Victor Andrade <victor64gaming@gmail.com>
2025-01-09 22:07:33 -0300

    Initial commit

---
+++ src/main/java/com/victoandrad/Electronics_Store/ElectronicsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.victoandrad.Electronics_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victoandrad/Electronics_Store/ElectronicsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoandrad.Electronics_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
